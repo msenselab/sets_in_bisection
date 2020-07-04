c6d1a1a97259c49e75b657fae0dafc1acc7b4c7a
XIUNA <fiona.zhu1230@gmail.com>
2020-07-04 12:26:47 +0200

    upload bisection_exp1 code

--- code/bisection_exp1.m
+++ code/bisection_exp1.m
@@ -1,86 +1,63 @@
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Name:          bisection_S3.m 
+%% Name:          bisection_exp1.m
 %%
-%% Author:         Fiona Zhu
-%% Description:   Main function for running the bisection study 2 to
-%% investigate the influence of ensemble variance on temporal bisection 
+%% Author:        Fiona Zhu
+%% Description:   Main function for running the bisection study 1 to
+%% investigate the distribution while keeping the spacing the same.
 %%
-%% note: 
-%% two types of sampled distributions will be tested: 
-%%       a normal, and a U-shaped distribution 
-%% 8 intervals (400,  550, 700, 850, 1000, 1150, 1300, and 1450 ms)
-%% Two practice blocks : uniform distribution 
-%% Author: Xiuna Zhu (Fiona)
-%% Date: 22-1-2018
-%% Contact: fiona.zhu1230@gmail.com
+%% note:
+%%      positive skew condition (400, 504, 636, 800, 1008, 1270, and 1600 ms)
+%%      negative skew condition (400, 730, 992, 1200, 1366, 1496, and 1600 ms)
+%%      two types of condition will be tested
+%% Date:          17-11-2018
+%% Modified by Xiuna Zhu (Fiona)
+%% Contact:   fiona.zhu1230@gmail.com
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function bisection_S3
+function bisection_exp1
 % -------------------------------------
-% This is the code for bisection task to study2 with a U-shaped
-% distribution and normal distribution
-% weight = [1/8      1/8    1/8      1/8     1/8     1/8     1/8      1/8;  %uniformal
-%          30/72    2/72    2/72     2/72    2/72    2/72    2/72    30/72;  %U-shaped
-%          2/72    2/72     2/72    30/72    30/72   2/72    2/72    2/72 ]; %normal
-%Participants have to identify the presented visual stimulus was close to the short or to the long
-% note:
+% This is an example code for bisection task to study frequency distribution
+% Participants have to identify the presented visual stimulus was close to the short or to the long
+% note§:
 %--------------------------------------
 
 try
-    close all;  clc;
-    num_dur = 8;
+    num_dur = 7;
     pracBlockNum = 2;
-    num_dur_all = 1/2*num_dur*(num_dur+1); %36
-    blockTrials =num_dur_all*2; % number of trials per block 72
-    exp = CExp(2,[1 blockTrials], 'blockRepetition', 2, 'blockFactors', 2);
-    %1.    
-    %2. duration 
-    %3. distribution: 1 = uniform; 2 = U-shaped; 3 = normal. 
+    num_dur_all = 1/2*num_dur*(num_dur+1);
+    blockTrials =num_dur_all*2; % number of trials per block (56 trials)
     
+    % block factor : frequency distribution A or B
+    % A: positive skew condition
+    % B: negative skew condition
+    exp = CExp(2,[num_dur_all],'blockRepetition', 6, 'blockFactors', 2);  %672 trials
+    %1.modality(only audio) 2. duration  3. frequency distribution A or B
     
-    idx = find(exp.seq(:,3)==1);
-    exp.seq(idx,3) = 3;
-    
+    %parameters
     frequency = 1000;
-    durations = [400.0, 550.0, 700.0, 850.0, 1000.0, 1150.0, 1300.0, 1450.0]/1000;
-    durList = ones(3, blockTrials);
-
-    % parameters for uniform distribution
-    for i = 1: num_dur
-        for g = 1 : blockTrials/num_dur
-            j = i+(g-1)*num_dur;
-            durList(1, j) = durations(i);  
-        end 
-    end
-    
-    % parameters for U-shaped distribution
-    weight_u = [30 2 2 2 2 2 2 30];
-    index = 0;
-    for i = 1: num_dur
-        for g = 1 : weight_u(i)
-            durList(2,index+g) = durations(i);  
-        end 
-        index = index + weight_u(i);
-    end
-    %currmean = sum(durations.*weight_u/blockTrials); %code to check if mean == 0.9250
-    %[mu,s,muci,sci] = normfit(durList(2,:)) %sigma = 0.4940
-
-    % parameters for normal distribution
-    weight_nor = [2 2 2 30 30 2 2 2];
-    index = 0;
-    for i = 1: num_dur
-        for g = 1 : weight_nor(i)
-            durList(3, index+g) = durations(i);  
-        end 
-        index = index + weight_nor(i);
-    end
-       %currmean = sum(durations.*weight_nor/blockTrials); %code to check if mean == 0.9250
-     %[mu,s,muci,sci] = normfit(durList(3,:)) %sigma = 0.1762
+    dur =  [400.0, 504.0, 636.0, 800.0, 1008.0, 1270.0, 1600.0;
+        400.0, 730.0, 992.0, 1200.0, 1366.0, 1496.0, 1600.0]/1000;
     
     
-        
-   % enquire subject information
-    exp.subInfo('U: 1(y) or 2(N)?)', '1');
+    weigth = [1   1     1     1   1   1   1];
+    currmean1 = sum(dur(1,:).*weigth/7); %code to check if mean == 0.8883
+    currmean2 = sum(dur(2,:).*weigth/7); %code to check if mean == 1.1120
+    x_mean1_2 = (dur(1,:)-currmean1).^2;
+    x_mean2_2 = (dur(2,:)-currmean2).^2;
+    sigma1 = sqrt(sum(x_mean1_2.*weigth/7)); %sigma1 =  0.4007
+    sigma2 = sqrt(sum(x_mean2_2.*weigth/7)); %sigma2 =  0.4009
+    
+    
+    
+    meanDur = 1;
+    durList = repmat(dur,1,4);
+  
+    
+    % mean of first set mean(dur(1,:)) = 0.8883, and mean of second set mean(dur(2,:)) = 1.1120
+    
+    % enquire subject information
+    exp.subInfo('Positive: 1(Y) or 2(N)?)', '1');
     if(strcmp(exp.sName, 'cancelled'))
         disp('This experiment has been cancelled!')
         return;
@@ -88,55 +65,73 @@ try
     
     %set the order
     if(exp.sPara == 1)
-        exp.seq = sortrows(exp.seq, 3);  %first U-shaped distribution
+        exp.seq = sortrows(exp.seq, 2);  %A: positive skew condition firstly
     else
-        exp.seq = sortrows(exp.seq, -3);  %first normal distribution
+        exp.seq = sortrows(exp.seq, -2); %B: negative skew condition firstly
     end
-   
     
-    % new two practice blocks (uniform distribution)
-    prac_seq = exp.genTrials(2,[1 blockTrials], 1);
+    % new two practice blocks
+    if pracBlockNum > 0
+        prac_seq = exp.genTrials(2,[num_dur_all], pracBlockNum);
+        %add 2 practice blocks' trials at beginning of part 1
+        exp.seq = [prac_seq(1:blockTrials*pracBlockNum,:); exp.seq];
+        exp.maxTrls = exp.maxTrls + blockTrials*pracBlockNum;
+    end
     
-    % add 2 practice blocks' trials at beginning of part 1
-    exp.seq = [prac_seq(1:blockTrials*pracBlockNum,:); exp.seq];
-    exp.maxTrls = exp.maxTrls + blockTrials*pracBlockNum;
     
     % input device
     kb=CInput('k',[0 1],{'leftArrow','rightArrow'});
     
     % audio device
-    a = CAudio; 
+    %devices = PsychPortAudio('GetDevices' );
+    %Option1: Using the computer's default audio device
+    a = CAudio;
+    %Option2: Using the Motu's audio device
+    %a = CAudiom('hardware', 'Motu', 'channels',2); % 2 channels
+    
     
     % visual display
     % v = CDisplay('bgColor',48,'fontSize',18, 'skipSync',1); % for test
-    v = CDisplay('bgColor',48,'fontSize',18,'monitorSize',22, 'fullWindow', 0);
+    v = CDisplay('bgColor',48,'fontSize',18,'monitorSize',22, 'fullWindow', 1);
     
-    HideCursor;
     
     infoText = init_text;
-    
-    noisedur = 2*durations(end);
-    noise = a.genPinkNoise(frequency, noisedur,-50)/noisedur; % 60 dBA
-    tone =  a.genTone(frequency*4, noisedur)*1.6; % 60 dBA 
-    tone_s = tone+noise; %mixing noise and tone in stumuli %307200
+    noisedur = 2*dur(end);
+    noise = a.genPinkNoise(frequency/2,noisedur,-50)/noisedur; % 60 dBA
+    tone =  a.genTone(frequency*4, noisedur)*1.6; % 60 dBA paras: obj, freq, duration
+    tone_s = tone+noise; %mixing noise and tone in stumuli
     tone_mix = noise;  % 2*144000
     warning = a.genTone(4000,0.02); % a warning signal
     
-    blockText = ['\n There are ' int2str(pracBlockNum) ' practice blocks, and '...
-    int2str(exp.maxTrls/blockTrials) ' blocks in total.\n\n'];
+    % display instructions and wait for keypress
+    pracTex = '\n There are ';
+    if pracBlockNum == 1
+        pracTex =[pracTex  'one practice block,'];
+    elseif pracBlockNum > 1
+        pracTex = [pracTex int2str(pracBlockNum) ' practice blocks, and '];
+    end
+    
+    blockText = [pracTex int2str(exp.maxTrls/blockTrials) ' blocks in total.\n\n'];
     v.dispText([infoText.instruction, blockText, infoText.startBlock]);
+    
     kb.wait;
     for iTrl=1:exp.maxTrls
-        if (mod(iTrl, blockTrials) == 1) && (iTrl == 0.5 * exp.maxTrls + 1) % session info
+        if pracBlockNum >= 1 && mod(iTrl, blockTrials) == 1 && floor(iTrl/blockTrials) < pracBlockNum
+            v.dispText(['Practice Block ' num2str(floor(iTrl/blockTrials)+1) ' \n Please press any key to start']);
+            kb.wait;
+        elseif (mod(iTrl, blockTrials) == 1) && (floor(iTrl/blockTrials) == (exp.maxTrls/(2*blockTrials) + pracBlockNum))
+            % show session info at block 16
             v.dispText(infoText.startSession);
             kb.wait;
-        elseif mod(iTrl, blockTrials) == 1  % block info
-            txtBlockInfo = ['Block ' num2str(floor(iTrl/blockTrials)+1) '\n' infoText.startBlock];
-            v.dispText(txtBlockInfo);
+        elseif mod((iTrl- pracBlockNum*blockTrials-1), blockTrials) == 0
+            v.dispText(['Block ' num2str(floor((iTrl-pracBlockNum*blockTrials)/blockTrials)+1) '\nPlease press any key to start this block']);
             kb.wait;
         end
         
+        
         cond = exp.getCondition;
+        targetDur = durList(cond(2), cond(1));
+        
         % fixtion with a warning tone
         v.dispFixation(1);
         a.prepare(warning);
@@ -145,26 +140,25 @@ try
         a.stop;
         
         %present audio
-        %presentAudio(a, tone_s, durList(cond(2)));
-
+        
         fixdur = 0.3+0.3*rand;%random 0.3-0.6s fixation duration
         tone_start_idx = round((fixdur)*a.freq);  %28800- 57600
-        tone_end_idx = durList(cond(3), cond(2))*a.freq + tone_start_idx-1;
-        mix_range = tone_start_idx:tone_end_idx; % 38400
-        tone_mix(:,mix_range)= tone_s(:,mix_range); %2*307200
+        tone_end_idx = targetDur*a.freq + tone_start_idx-1;
+        mix_range = tone_start_idx:tone_end_idx;
+        tone_mix(:,mix_range)=tone_s(:,mix_range);
         a.prepare(tone_mix);
         a.present;
-        WaitSecs(0.3 + durList(cond(3), cond(2))); % keep same for all trials
+        WaitSecs(0.3 + targetDur); % keep same for all trials
         a.stop; %stop when the sound is finished
         %WaitSecs(0.3);
         v.flip(1);
         %acquire response
         v.dispText(infoText.question);
-        key= kb.response;
-        if iTrl <= blockTrials*pracBlockNum
+        key = kb.response;
+        if iTrl <= blockTrials*2
             % feedback
             feedbacktext = infoText.short;
-            if durList(cond(3), cond(2)) >= durations(floor(num_dur/2) +1)
+            if targetDur  >  meanDur
                 feedbacktext = infoText.long;
             end
             v.dispText(feedbacktext);
@@ -172,7 +166,7 @@ try
         end
         
         % store duration and responses
-        exp.setResp([durList(cond(3), cond(2)) key]);
+        exp.setResp([targetDur key]);
         v.flip(1); %clear screen
         % inter trial interval (ITI)
         WaitSecs(0.9+0.2*rand);
@@ -187,6 +181,7 @@ try
             break;
         end
     end
+    
     
     %closing the experiment
     exp.saveData;   %save data
@@ -211,8 +206,8 @@ end
 function infoText = init_text
 % specify experimental text
 infoText.instruction = ['Duration Bisection Experiment \n Please follow the instruction the experimenter gave.\n',...
-    ' In this experiment, you will hear a noise after a tone . \nYour task is to judge if the noise is presented for a short or long time.\n', ...
-    ' During practice blocks, there is feedback telling you the right answer after each trial.'];
+    ' In this experiment, you will hear a tone then a noise. \nYour task is to judge if the noise presented is a short or long stimuli.\n', ...
+    ' You will learn what long or short is after few trials.'];
 infoText.practice = 'This is the practice block \n Press a key to begin';
 infoText.formalInfo = 'The formal experiment will start soon, \n please press a key to start\n';
 infoText.blockInfo = ['Please take a rest, and when you are ready, please press a key to start a new block. \n ', ...
@@ -224,7 +219,7 @@ infoText.startBlock = 'Please press any key to start this block';
 infoText.thankyou = 'The experiment is finished! \nThank you very much!';
 infoText.short = 'The given stimulus is short!';
 infoText.long = 'The given stimulus is long!';
-infoText.startSession = ['The first part was finished. \n '...
+infoText.startSession = ['The first part was finished. Please take a longer break. \n '...
     'When you are ready, please press a key to start the second part. \n ', ...
     'Your task is to judge if the given stimulus is short (left arrow key) or long (right arrow key)'];
 
